a465931ec9309f1c440a50f2c5a1e4433dd546da
Dean Alvero <deanalvero@gmail.com>
2025-12-28 22:41:31 -0500

    MoveRecord

---
+++ composeApp/src/commonMain/kotlin/io/github/deanalvero/tictactoefamily/model/MoveRecord.kt
@@ -0,0 +1,10 @@
+package io.github.deanalvero.tictactoefamily.model
+
+data class MoveRecord(
+    val player: Player,
+    val pieceRank: Int,
+    val from: String,
+    val to: String
+) {
+    override fun toString(): String = "${player.text} (Rank $pieceRank): $from → $to"
+}
